493b5e0d2a2f55acd20c02b2f27b343272c58a03
Karisha:) <krshmelendez@gmail.com>
2026-05-22 14:07:49 +0200

    added book component and page to main

--- src/pages/bookPage/Book.jsx
+++ src/pages/bookPage/Book.jsx
@@ -1,11 +1,11 @@
 import BookCard from "../../components/bookCard/Book";
-
+import Button from "../../components/button/Button";
 
 function Book(){
     return(
         <main>
         
-
+        <Button/>
         <BookCard/>
         
         </main>
